999bb8f7e7644e8f7b3411ea4e47e19dea4649e8
hyogrin <hyogrin@gmail.com>
2024-11-14 11:40:59 +0900

    Jinja2 template based connection replacer

--- Promptflow with Python SDK/chat-serverless/phi35_chatcompletion.py
+++ Promptflow with Python SDK/chat-serverless/phi35_chatcompletion.py
@@ -26,18 +26,16 @@ def chat(question: str, context:str, connection: CustomConnection) -> str:
     data = {
         "messages": 
             [
-                {"role": "user", "content": "You are an AI assistant who helps people find information. As the assistant, you answer questions not long, simple, short. Add a witty joke that begins with By the way, or By the way. The joke should be related to the specific question asked. For example, if the question is about tents, the joke should be specifically related to tents."},
+                {"role": "user", "content": "You are an AI assistant who helps people find information. As the assistant, you answer questions not long, simple, short."},
                 {"role": "user", "content": "Use the following context to provide a more personalized response to the customer:"},
                 {"role": "user", "content": context},
-                {"role": "user", "content": "tell me about your TrailMaster X4"},
-                {"role": "assistant", "content": "The TrailMaster X4 is a rugged four-wheel off-road vehicle with a powerful engine and durable frame. "},
                 {"role": "user", "content": question},
-                {"role": "user", "content": "Answer in Korean language."},
+                {"role": "user", "content": "Answer in Korean."},
                 
             ],
         "parameters": {
-                "temperature": 0.7,
-                "max_new_tokens": 1024,
+                "temperature": 0.2,
+                "max_new_tokens": 256,
                 "do_sample": True,
                 "return_full_text": False
         }
